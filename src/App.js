d864c3b12248e6876736a7be11df126bd004926a
MD DANIYAL HASAN <117173402+daniyalamu@users.noreply.github.com>
2024-09-06 18:32:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,32 +9,21 @@ function App() {
     <>
       <GlobalStyles styles={{ body: { backgroundColor: '#1E1E1E', color: 'white', margin: 0, padding: 0 } }} />
       <div className="App" style={{ height: '100vh' }}>
-        <header className="App-header">
-          {/* Header content if needed */}
-        </header>
-        
+        <header className="App-header"></header>
         <Grid container spacing={2} style={{ height: '100%' }}>
-          {/* Paragraph component on the left side taking up 50% */}
           <Grid item xs={6}>
             <div style={{ backgroundColor: '#616161', border: '1px solid #96BEE7', height: '94%', padding: '16px' }}>
               <Paragraph />
             </div>
           </Grid>
-
-          {/* Right side split between Tabs and Gallery */}
           <Grid item xs={6}>
             <Grid container direction="column" style={{ height: '100%' }}>
-              {/* Tabs component taking up 50% of the right side */}
               <Grid item style={{ height: '49%', backgroundColor: '#363C43', position: 'relative' }}>
                 <Tabs />
               </Grid>
-              
-              {/* Divider with a gap between Tabs and Gallery */}
               <Grid item style={{ height: '2%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Divider style={{ width: '80%', borderBottom: '1px solid white' }} />
               </Grid>
-
-              {/* Gallery component taking up 50% of the right side */}
               <Grid item style={{ height: '49%', backgroundColor: '#363C43', position: 'relative' }}>
                 <Gallery />
               </Grid>
